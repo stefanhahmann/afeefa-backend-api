b36a44ead72c6ea9dac994f436d1296d36d53430
hahmann <stefan.hahmann@ivi.fraunhofer.de>
2018-10-05 14:13:42 +0200

    Replace Google Geocoding by Nominatim (=OpenStreetMap) Geocoding (untested!)
    - Use gem 'nominatim' for querying addresses from OpenStreetMap address
    search server
    - Doc: see https://www.rubydoc.info/gems/nominatim 
    https://rubygems.org/gems/nominatim/versions/0.0.6
    https://www.rubydoc.info/gems/nominatim/Nominatim/Place
    https://www.rubydoc.info/gems/nominatim/Nominatim/Address
    - Contact e-mail address needs to be added in geocoder.rb
    - Needs to be tested by someone who knows how to run ruby
    (unfortunately, I am not at all an expert in Ruby)

--- app/controllers/api/v1/geocodings_controller.rb
+++ app/controllers/api/v1/geocodings_controller.rb
@@ -4,15 +4,30 @@ class Api::V1::GeocodingsController < ApplicationController
 
   def index
     location = Location.new(geocoding_params)
-    results = Geocoder.search(location.address_for_geocoding)
-    if results && results.any? && result = results.first
-      street = result.address.split(',').first
+    # address search using google geocoder
+#    results = Geocoder.search(location.address_for_geocoding)
+#    if results && results.any? && result = results.first
+#      street = result.address.split(',').first
+#      render json: {
+#        latitude: result.latitude,
+#        longitude: result.longitude,
+#        street: street,
+#        city: result.city,
+#        full_address: result.address
+#      }
+    
+    # address search using nominatim (openstreetmap) geocoder
+    # see https://www.rubydoc.info/gems/nominatim
+    # see https://www.rubydoc.info/gems/nominatim/Nominatim/Place
+    # see https://www.rubydoc.info/gems/nominatim/Nominatim/Address
+    places = Nominatim.search(location.address_for_geocoding).limit(1).address_details(true)
+    if places && places.any? && place = places.first
       render json: {
-        latitude: result.latitude,
-        longitude: result.longitude,
+        latitude: place.lat,
+        longitude: place.lon,
         street: street,
-        city: result.city,
-        full_address: result.address
+        city: place.address.city,
+        full_address: place.address
       }
     else
       render json: 'geocoding failed for address', status: :unprocessable_entity
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -5,3 +5,14 @@ Geocoder.configure(
   cache: {},
   always_raise: :all
 )
+
+Nominatim.configure do |config|
+  # TODO enter valid afeefa e-mail address
+  # https://wiki.openstreetmap.org/wiki/DE:Nominatim#Parameter
+  # "Wenn sie eine große Anzahl an Suchabfragen abschicken wollen, geben sie bitte ihre Mail-Adresse in der Abfrage an.
+  # "Diese Info wird vertraulich behandelt und nur verwendet um mit ihnen bei Problemen in Kontakt treten zu können. Siehe auch unsere Usage-Policy."
+  config.email = '@afeefa.de'
+  config.endpoint = 'https://nominatim.openstreetmap.org/search'
+  config.endpoint = 'https://nominatim.openstreetmap.org'
+end
+
